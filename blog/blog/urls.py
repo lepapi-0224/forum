a739f05b501602d34fe90539b055d6046c6014ed
lepapi-0224 <navelie2013@gmail.com>
2022-03-19 13:26:40 +0100

    feat make view with html5

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('home', views.home, name='home'),
 ]
