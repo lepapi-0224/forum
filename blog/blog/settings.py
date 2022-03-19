5de9a490800bef74cb1668cbe32b694cb2d100e6
lepapi-0224 <navelie2013@gmail.com>
2022-03-19 11:15:19 +0100

    make model and migrate to database

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,8 +79,15 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '5432',
+        'ATOMIC_REQUESTS': True,
+        'AUTOCOMMIT': True,
+        'CONN_MAX_AGE': 60,
     }
 }
 
--- blog/core/models.py
+++ blog/core/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Board(models.Model):
+    name = models.CharField(
+        max_length=30,
+        null=False,
+        blank=False,
+        unique=True
+    )
+    description = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True
+    )
+
+
+class Topic(models.Model):
+    subject = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+    last_updated = models.DateTimeField(
+        auto_now_add=True
+    )
+    board = models.ForeignKey(
+        Board,
+        related_name='topics',
+        on_delete=models.DO_NOTHING
+    )
+    starter = models.ForeignKey(
+        User,
+        related_name='topics',
+        on_delete=models.DO_NOTHING
+    )
+
+
+class Post(models.Model):
+    message = models.TextField(
+        max_length=4000,
+        null=False,
+        blank=False
+    )
+    topic = models.ForeignKey(
+        Topic,
+        related_name='posts',
+        on_delete=models.DO_NOTHING
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        null=True
+    )
+    created_by = models.ForeignKey(
+        User,
+        related_name='posts',
+        on_delete=models.DO_NOTHING
+    )
+    updated_by = models.ForeignKey(
+        User,
+        null=True,
+        related_name='+',
+        on_delete=models.DO_NOTHING
+    )
