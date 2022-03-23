52a6ad7eda9553f10d4298f47345656d2172e8cf
lepapi-0224 <navelie2013@gmail.com>
2022-03-23 11:15:00 +0100

    write view topics.html

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('boards/', views.board_topics, name='board_topics'),
+    path('admin/', admin.site.urls),
 ]
--- blog/core/views.py
+++ blog/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Board
 
+
 # Create your views here.
 
 
@@ -8,3 +9,7 @@ def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
 
+
+def board_topics(request, pk):
+    board = Board.objects.get(pk=pk)
+    return render(request, 'topics.html', {'board': board})
